028925971eb29295c8eb783deb3d0c6315ff6870
Przemysław <p.d.walczak@gmail.com>
2021-07-22 09:00:07 +0200

    Version 1.0.1

--- example/dependency.ts
+++ example/dependency.ts
@@ -3,12 +3,12 @@ import { Context, Allocation } from '../gameloop'
 const test = async () => {
   const start = process.hrtime.bigint()
 
-  await Allocation.extend('data/item', 1024 * 1024 * 1)
+  await Allocation.extend('data/item', 1024 * 1024 * 128)
   .catch(e => {
     console.log(e)
   })
 
-  // console.log('extension took:', parseInt((process.hrtime.bigint() - start).toString()) / 1000000, 'ms')
+  console.log('extension took:', parseInt((process.hrtime.bigint() - start).toString()) / 1000000, 'ms')
   console.log('extended to:', Allocation.get('data/item').buffer?.byteLength)
 
   await new Promise((resolve, reject) => setTimeout(() => {
