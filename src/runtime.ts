c724cd89aef10f3ae29db968756b764ba80e43b3
Przemysław <p.d.walczak@gmail.com>
2021-07-25 12:56:48 +0200

    Version 1.0.22 - Removed Runtime.Constructor() due to lack of proper extension of constructor from class property.

--- src/runtime.ts
+++ src/runtime.ts
@@ -21,32 +21,13 @@ export function Hook(args: any[] = [])
   }
 }
 
-export function Constructor(args: any[] = [])
-{
-  return (target: any, propertyKey: string) => {
-    const hook = propertyKey ? target[propertyKey] : target
-
-    if (typeof hook !== 'function')
-    {
-      throw new Error(`Runtime.Hook() Requires a valid property type of function`)
-    }
-
-    const constructor = target.constructor
-
-    if (typeof constructor !== 'function')
-    {
-      throw new Error(`Runtime.Constructor() Requires a valid target constructor function`)
-    }
-
-    target.constructor = function Runtime (..._args: any[]) {
-      constructor(..._args)
-
-      Stack.push({ hook, target: this, args })
-    }
-  }
-}
-
 export async function Flush()
 {
-  
+  return await Promise.all(
+    Stack.map(
+      ({ hook, target, args }) => {
+        return hook.apply(target, args)
+      }
+    )
+  )
 }
